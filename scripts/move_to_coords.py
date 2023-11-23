1329d408b8b5cad28cd2c1a67b278c770ea504e6
chit <example@mail.com>
2023-11-23 14:14:26 +0000

    change move_to_coords to accept coords message

--- scripts/move_to_coords.py
+++ scripts/move_to_coords.py
@@ -3,13 +3,15 @@ import rospy
 from geometry_msgs.msg import Twist, PoseWithCovarianceStamped
 import math
 import time
-from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import Quaternion, Pose
+from std_msgs.msg import String
 
 
 COORD_TO_MOVE_TO = (17, 4)
 message = None
 TOLERANCE = 0.2
 DISTANCE_TOLERANCE = 0.5
+told_about_finished = False
 
 
 def pose_callback(msg):
@@ -18,7 +20,15 @@ def pose_callback(msg):
     print("got message")
 
 
+def move_to_goal_callback(msg):
+    global COORD_TO_MOVE_TO, told_about_finished
+    COORD_TO_MOVE_TO = (msg.position.x, msg.position.y)
+    told_about_finished = False
+    print("got new goal")
+
+
 def move():
+    global message, told_about_finished
     if not message:
         return
     # coords
@@ -28,6 +38,11 @@ def move():
     # calculate the distance to the target
     distance = math.sqrt((COORD_TO_MOVE_TO[0] - x) ** 2 + (COORD_TO_MOVE_TO[1] - y) ** 2)
     if distance < DISTANCE_TOLERANCE:
+        if not told_about_finished:
+            string_data = String()
+            string_data.data = "finished"
+            move_to_coords_pub.publish(string_data)
+            told_about_finished = True
         return
 
     # calculate the yaw to the target
@@ -38,29 +53,31 @@ def move():
         print("turn left")
         base_data = Twist()
         base_data.angular.z = 0.3
-        base_data.linear.x = 0.2
-        pub.publish(base_data)
+        # base_data.linear.x = 0.2
+        movement_pub.publish(base_data)
     elif (target_yaw - yaw) < -TOLERANCE:
         # turn right
         print("turn right")
         base_data = Twist()
         base_data.angular.z = -0.3
-        base_data.linear.x = 0.2
-        pub.publish(base_data)
+        # base_data.linear.x = 0.2
+        movement_pub.publish(base_data)
     else:
         # move forward
         print("move forward")
         base_data = Twist()
         base_data.linear.x = 0.4
-        pub.publish(base_data)
+        movement_pub.publish(base_data)
 
 
 
 def main():
     rospy.init_node('move_to_coords')
-    global pub
-    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=100)
+    global movement_pub, move_to_coords_pub
+    movement_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=100)
+    move_to_coords_pub = rospy.Publisher('/move_to_coords', String, queue_size=100)
     rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped, pose_callback)
+    rospy.Subscriber('/move_to_goal', Pose, move_to_goal_callback)
 
     while not rospy.is_shutdown():
         move()
