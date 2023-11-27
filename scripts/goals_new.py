7b6f0b2c7be82af88b73977bb93af1f2cab69275
lizzzzzok <lizzzzzok@Ubuntu20.04.myguest.virtualbox.org>
2023-11-27 23:35:15 +0000

    Modified waypoints

--- scripts/goals_new.py
+++ scripts/goals_new.py
@@ -9,38 +9,31 @@ rospy.init_node('navigate_robot')
 
 # Updated list of goals with your coordinates
 goals = [
-    Pose(position=Point(x=3.039054575402686, y=3.739731769616226, z=0.0),
-         orientation=Quaternion(w=0.8337602955527833, x=0.0, y=0.0, z=0.5521265883470342)),
-    Pose(position=Point(x=1.9327985587137941, y=11.423447682767932, z=0.0),
-         orientation=Quaternion(w=0.9201964048699748, x=0.0, y=0.0, z=0.39145699184504723)),
-    Pose(position=Point(x=15.11636640422266, y=11.425471330763873, z=0.0),
-         orientation=Quaternion(w=0.997910297702185, x=0.0, y=0.0, z=0.06461453195633741)),
-    Pose(position=Point(x=12.905579134804732, y=7.9848322620675525, z=0.0),
-         orientation=Quaternion(w=0.980740448390913, x=0.0, y=0.0, z=-0.19531557257420862)),
-    Pose(position=Point(x=9.504558737679664, y=12.853681794445112, z=0.0),
-         orientation=Quaternion(w=0.9478868168720372, x=0.0, y=0.0, z=-0.31860725415501284)),
-    Pose(position=Point(x=14.295679704866666, y=21.880189496153275, z=0.0),
-         orientation=Quaternion(w=0.9999562584662598, x=0.0, y=0.0, z=0.009353136060097923)),
-    Pose(position=Point(x=12.955258052470636, y=23.688073509473423, z=0.0),
-         orientation=Quaternion(w=0.9775336959333584, x=0.0, y=0.0, z=0.2107792051291313)),
-    Pose(position=Point(x=16.279586618366, y=26.657067088902657, z=0.0),
-         orientation=Quaternion(w=0.9268607199649388, x=0.0, y=0.0, z=0.37540538859488315)),
-    Pose(position=Point(x=16.958741068978618, y=21.78485755495617, z=0.0),
-         orientation=Quaternion(w=0.9578953891383724, x=0.0, y=0.0, z=-0.28711743845932836)),
-    Pose(position=Point(x=18.948990828185984, y=17.857989217411323, z=0.0),
-         orientation=Quaternion(w=0.9674684317201359, x=0.0, y=0.0, z=-0.25299176592328215)),
-    Pose(position=Point(x=26.60789029586225, y=17.77152826708097, z=0.0),
-         orientation=Quaternion(w=0.9999915213696485, x=0.0, y=0.0, z=0.004117910734322616)),
-    Pose(position=Point(x=27.33880758748402, y=27.158517770018815, z=0.0),
-         orientation=Quaternion(w=0.9264104237715857, x=0.0, y=0.0, z=-0.376515241029299))
-    # You can add more poses here in the same format
+    Pose(position=Point(x=2.139872800488411, y=2.4091084324016747, z=0.0),
+         orientation=Quaternion(w=0.9999750369921838, x=0.0, y=0.0, z=0.007065790294124714)), 
+    Pose(position=Point(x=1.7942427343093557, y=10.997075823076422, z=0.0),
+         orientation=Quaternion(w=0.6945051411862567, x=0.0, y=0.0, z=0.7194877405945549)),
+    Pose(position=Point(x=14.4578006343115, y=10.575440599835618, z=0.0),
+         orientation=Quaternion(w=0.9995247774133863, x=0.0, y=0.0, z=0.03082562792094638)),  
+    Pose(position=Point(x=13.689259857644291, y=21.31942142179531, z=0.0),
+         orientation=Quaternion(w=0.6917303017201268, x=0.0, y=0.0, z=0.722155931694937)),
+    Pose(position=Point(x=17.286134723080238, y=22.362174253978868, z=0.0),
+         orientation=Quaternion(w=0.9942319132574566, x=0.0, y=0.0, z=0.10725158581772617)),
+    Pose(position=Point(x=12.420124525361263, y=23.583000051064765, z=0.0),
+         orientation=Quaternion(w=0.2607200755719132, x=0.0, y=0.0, z=0.9654144406387217)),
+    Pose(position=Point(x=4.485485112377619, y=13.62544721833074, z=0.0),
+         orientation=Quaternion(w=0.9426620672762931, x=0.0, y=0.0, z=0.33374874818999023)),
+    Pose(position=Point(x=12.377816896015693, y=7.411229829575238, z=0.0),
+         orientation=Quaternion(w=0.5067124484646074, x=0.0, y=0.0, z=-0.8621151283738168)),
+    Pose(position=Point(x=22.86934957609736, y=8.080257281520984, z=0.0),
+         orientation=Quaternion(w=0.9440032004771164, x=0.0, y=0.0, z=-0.3299362930763473)),  
 ]
 
-Table_4 = [goals[1], goals[2], goals[3]]
-Table_5 = [goals[1], goals[2], goals[4]]
-Table_3 = [goals[1], goals[2], goals[5]]
-Table_2 = [goals[1], goals[2], goals[6], goals[7]]
-Table_1 = [goals[1], goals[2], goals[6], goals[9]]
+Table_4 = [goals[1], goals[2], goals[7]]
+Table_5 = [goals[1], goals[2], goals[8]]
+Table_3 = [goals[1], goals[6]]
+Table_2 = [goals[1], goals[2], goals[3], goals[5]]
+Table_1 = [goals[1], goals[2], goals[3], goals[4]]
 
 goal_pub = rospy.Publisher('/move_to_goal', Pose, queue_size=10)
 
